90faeda9327504e7c58805ebb7008751cc3a0b24
Kevin Thibedeau <6765123+kevinpt@users.noreply.github.com>
2022-12-20 17:16:43 -0500

    Clean up elf_patch arg processing

--- tools/elf_patch/src/elf_patch.c
+++ tools/elf_patch/src/elf_patch.c
@@ -347,10 +347,8 @@ bool firmware_crc_from_elf(Elf *elf, ObjMemRegion *regions, SegmentIndex *seg_in
 }
 
 
-#define CHAR_PASS   A_BGRN u8"✓" A_NONE
-#define CHAR_FAIL   A_BRED u8"✗" A_NONE
-
-int generate_crcs(const char *elf_file, const char *bin_file, bool verbose, bool apply_update) {
+int generate_crcs(const char *elf_file, const char *bin_file, bool verbose, bool apply_update,
+                  bool strip_crc, bool colorize) {
   int rval = 0;
 
   int fd = open(elf_file, apply_update ? O_RDWR : O_RDONLY);
@@ -395,9 +393,17 @@ int generate_crcs(const char *elf_file, const char *bin_file, bool verbose, bool
 
 
   if(apply_update) {
-    puts("Updating CRCs");
-    obj_meta->obj_crc = obj_crc;
-    obj_meta->meta_crc = meta_crc;
+    if(!strip_crc) {
+      printf("Updating CRCs in %s\n", elf_file);
+      obj_meta->obj_crc = obj_crc;
+      obj_meta->meta_crc = meta_crc;
+
+    } else {
+      printf("Removing CRCs in %s\n", elf_file);
+      obj_meta->obj_crc = 0;
+      obj_meta->meta_crc = 0;
+    }
+
     elf_flagdata(meta_data, ELF_C_SET, ELF_F_DIRTY);
     elf_end(elf); // Commit updates
 
@@ -418,14 +424,23 @@ int generate_crcs(const char *elf_file, const char *bin_file, bool verbose, bool
   // Report info from metadata
   int major, minor, patch;
   firmware_version(obj_meta->obj_version, &major, &minor, &patch);
-  printf("\nApplication: %s  %d.%d.%d  Git SHA: %08x%s\n", obj_meta->obj_name,
-          major, minor, patch, obj_meta->git_sha,
-          obj_meta->debug_build ? A_RED "  DEBUG" A_NONE : "");
+  printf("\nApplication: %s  %d.%d.%d  Git SHA: %08x", obj_meta->obj_name,
+          major, minor, patch, obj_meta->git_sha);
 
-  printf("   App CRC: 0x%08X %s\n", obj_crc, obj_crc == obj_meta->obj_crc ? CHAR_PASS : CHAR_FAIL);
+#define COLORIZE(str, color, enable)  (enable) ? (color str A_NONE) : (str)
+  printf("%s\n", obj_meta->debug_build ? COLORIZE("  DEBUG", A_RED, colorize) : "");
+
+#define CHAR_PASS   u8"✓"
+#define CHAR_FAIL   u8"✗"
+
+  printf("   App CRC: 0x%08X", obj_crc);
+  printf(" %s\n", obj_crc == obj_meta->obj_crc ? COLORIZE(CHAR_PASS, A_BGRN, colorize) :
+                                                 COLORIZE(CHAR_FAIL, A_BRED, colorize));
 //  printf("\tGot: 0x%08X\n", obj_meta->obj_crc);
 
-  printf("  Meta CRC: 0x%04X %s\n", meta_crc, meta_crc == obj_meta->meta_crc ? CHAR_PASS : CHAR_FAIL);
+  printf("  Meta CRC: 0x%04X", meta_crc);
+  printf(" %s\n", meta_crc == obj_meta->meta_crc ? COLORIZE(CHAR_PASS, A_BGRN, colorize) :
+                                                 COLORIZE(CHAR_FAIL, A_BRED, colorize));
 //  printf("\tGot: 0x%04X\n", obj_meta->meta_crc);
 
   if(obj_crc != obj_meta->obj_crc || meta_crc != obj_meta->meta_crc) rval = ERR_VERIFY;
@@ -436,9 +451,36 @@ cleanup:
   return rval;
 }
 
+void show_help(char *argv[]) {
+  StringRange app_name;
+  path_basename(argv[0], &app_name);
+  printf(A_BOLD "Usage: " A_NONE
+        "%"PRISR" -i <ELF file> [-o <ELF file>] [-b <BIN file>] [-c] [-s] [-v] [-V] [-h]\n",
+        RANGE_FMT(&app_name));
+  puts(" Add CRCs to an ELF firmware image");
+  puts("  -i <ELF file>   Input firmware image");
+  puts("  -o <ELF file>   Output image");
+  puts("  -b <BIN file>   Binary firmware image");
+  puts("  -c              Check ELF file only");
+  puts("  -s              Strip CRCs in output");
+  puts("  -v              Verbose output");
+  puts("  -V              Report version");
+  puts("  -h              Show help");
+
+  printf("\n" A_BOLD "Example:\n" A_NONE
+         "  %"PRISR" -i firmware.elf                # Generate CRCs in firmware.elf\n",
+          RANGE_FMT(&app_name));
+  printf("  %"PRISR" -i firmware.elf -o fw_crc.elf  # Same with new image \n", RANGE_FMT(&app_name));
+  printf("  %"PRISR" -i firmware.elf -c -v          # Only check CRCs with detailed metadata\n", RANGE_FMT(&app_name));
+}
 
 int main(int argc, char *argv[]) {
   int status = 0;
+
+  if(argc == 1) {
+    show_help(argv);
+    return 0;
+  }
 
   GetoptState state = {0};
   state.report_errors = true;
@@ -449,33 +491,23 @@ int main(int argc, char *argv[]) {
   const char *elf_file = NULL;
   const char *out_file = NULL;
   bool check_elf = false;
+  bool strip_crc = false;
   bool verbose = false;
   bool show_version = false;
 
-  while((c = getopt_r(argv, "b:i:o:cvVh", &state)) != -1) {
+  while((c = getopt_r(argv, "b:i:o:csvVh", &state)) != -1) {
     switch(c) {
     case 'b': bin_file = state.optarg; break;
     case 'i': elf_file = state.optarg; break;
     case 'o': out_file = state.optarg; break;
     case 'c': check_elf = true; break;
+    case 's': strip_crc = true; break;
     case 'v': verbose = true; break;
     case 'V': show_version = true; break;
 
-    case 'h': {
-      StringRange app_name;
-      path_basename(argv[0], &app_name);
-      printf("Usage: %"PRISR" -i <ELF file> [-o <ELF file>] [-b <BIN file>] [-c] [-v] [-V] [-h]\n",
-            RANGE_FMT(&app_name));
-      puts(" Add CRCs to an ELF firmware image");
-      puts("  -i <ELF file>   Input firmware image");
-      puts("  -o <ELF file>   Output image");
-      puts("  -b <BIN file>   Binary firmware image");
-      puts("  -c              Check ELF file only");
-      puts("  -v              Verbose output");
-      puts("  -V              Report version");
-      puts("  -h              Show help");
+    case 'h':
+      show_help(argv);
       return 0;
-      }
       break;
 
     default:
@@ -496,6 +528,7 @@ int main(int argc, char *argv[]) {
     return ERR_MISSING_INPUT;
   }
 
+#if 0
   if(!bin_file) {
     size_t buf_len = strlen(elf_file)+4+1;
     bin_file_buf = malloc(buf_len);
@@ -513,9 +546,9 @@ int main(int argc, char *argv[]) {
 
     bin_file = bin_file_buf;
   }
+#endif
 
-
-  if(out_file && strcmp(elf_file, out_file) != 0) { // Copy input file
+  if(out_file && strcmp(elf_file, out_file) != 0 && !check_elf) { // Copy input file
     if(!copy_file(elf_file, out_file))
       return ERR_FILE_ACCESS;
     elf_file = out_file;
@@ -527,213 +560,26 @@ int main(int argc, char *argv[]) {
     return ERR_MISSING_INPUT;
   }
 
-  if(!file_exists(bin_file)) {
-    printf("No BIN file '%s'. Using ELF for CRC.\n", bin_file);
+  if(!bin_file || !file_exists(bin_file))
     bin_file = NULL;
-    //return ERR_MISSING_INPUT;
-  }
 
+  printf("Using %s for CRC\n", bin_file ? "BIN" : "ELF");
 
   elf_version(EV_CURRENT);  // Prepare libelf
 
 
-#if 0
-  int fd = open(elf_file, O_RDWR);
-  if(!fd)
-    return ERR_FILE_ACCESS;
+  // Color output for TTYs only
+  bool colorize = isatty(1);
 
+  char *no_color = getenv("NO_COLOR");
+  if(no_color && no_color[0] != '\0')
+    colorize = false;
 
-  Elf *elf = elf_begin(fd, ELF_C_RDWR_MMAP, NULL);
-  if(elf) {
-    elf_flagelf(elf, ELF_C_SET, ELF_F_LAYOUT);  // https://stackoverflow.com/questions/35949369/libelf-corrupts-arm-elf-binaries
+  status = generate_crcs(elf_file, bin_file, verbose, /*apply_update*/!check_elf, strip_crc, colorize);
 
-#  if 0    
-    puts("Got ELF");
-    Elf_Kind k = elf_kind(elf);
-    printf("Kind: %d\n", k);
-    if(k == ELF_K_ELF) {
-      size_t section_count, string_table_ix;
-      elf_getshdrnum(elf,  &section_count);
-      elf_getshdrstrndx(elf, &string_table_ix);
-      printf("Sections: %d\n", section_count);
-      printf("String table: %d\n", string_table_ix);
-      
-      size_t prog_hdr_count;
-      elf_getphdrnum(elf, &prog_hdr_count);
-      for(size_t i = 0; i < prog_hdr_count; i++) {
-        GElf_Phdr phdr;
-        if(gelf_getphdr(elf, i, &phdr) == &phdr) {
-          printf("PHDR: %d %s\n", i, prog_hdr_type(phdr.p_type));
-#define PRINT_FIELD(field)  printf("\t%s: 0x%jX  (%d)\n", #field, phdr.field, phdr.field);
-          PRINT_FIELD(p_type);
-          PRINT_FIELD(p_vaddr);
-          PRINT_FIELD(p_paddr);
-          PRINT_FIELD(p_filesz);
-          PRINT_FIELD(p_memsz);
-          fputs("\tflags: ", stdout);
-          if(phdr.p_flags & PF_X) fputs("X", stdout);
-          if(phdr.p_flags & PF_R) fputs("R", stdout);
-          if(phdr.p_flags & PF_W) fputs("W", stdout);
-          puts("");
-        }
-      }
-
-      // Read sections
-      Elf_Scn *scn = NULL;
-      GElf_Shdr shdr;
-      char *name;
-      //Elf_Data *data;
-      while((scn = elf_nextscn(elf, scn)) != NULL) {
-        if(gelf_getshdr(scn, &shdr) == &shdr) {
-          if((name = elf_strptr(elf, string_table_ix, shdr.sh_name)) != NULL) {
-            printf("Section %4jd %s\tsize= %d\n" , ( uintmax_t )elf_ndxscn(scn), name, shdr.sh_size);
-          }
-        }
-      }
-#  endif
-      Elf_Scn *meta_scn = get_named_section(elf, ".metadata");
-      if(meta_scn) {
-//        GElf_Shdr meta_hdr;
-//        gelf_getshdr(meta_scn, &meta_hdr);
-//        printf(".metadata size= %d\n", meta_hdr.sh_size);
-        Elf_Data *meta_data = NULL;
-//        while((meta_data = elf_getdata(meta_scn, meta_data))) {
-//          dump_array(meta_data->d_buf, meta_data->d_size);
-//        }
-
-//        meta_data = NULL;
-        meta_data = elf_getdata(meta_scn, meta_data);
-        ObjectMetadata *obj_meta = (ObjectMetadata *)meta_data->d_buf;
-        //memcpy(&obj_meta, meta_data->d_buf, meta_data->d_size);
-        //dump_array((uint8_t *)obj_meta, meta_data->d_size);
-
-/*
-        int major, minor, patch;
-        firmware_version(obj_meta->obj_version, &major, &minor, &patch);
-        printf("Obj name: %s, ver: %d.%d.%d sz: %d\n", obj_meta->obj_name,
-                major, minor, patch, sizeof obj_meta);
-        puts("Regions:");
-        int region = 0;
-        for(int i = 0; i < OBJ_MAX_REGIONS; i++) {
-          if(obj_meta->mem_regions[i].end - obj_meta->mem_regions[i].start == 0)
-            break;
-            
-          printf("\ts: %08X,  e: %08X,  sz: %d\n", obj_meta->mem_regions[i].start,
-                obj_meta->mem_regions[i].end,
-                obj_meta->mem_regions[i].end - obj_meta->mem_regions[i].start);
-        }
-*/
-        if(!firmware_crc(bin_file, obj_meta->mem_regions, &obj_meta->obj_crc))
-          status = ERR_FILE_ACCESS;
-        printf("CRC32: 0x%08X\n", obj_meta->obj_crc);
-
-        uint16_t meta_crc = crc16_init();
-        // Skip over initial CRCs in metadata struct
-        size_t meta_offset = offsetof(ObjectMetadata, meta_crc) + FIELD_SIZE(ObjectMetadata, meta_crc);
-        meta_crc = crc16_update_block(meta_crc, meta_data->d_buf + meta_offset,
-                                      meta_data->d_size - meta_offset);
-        meta_crc = crc16_finish(meta_crc);
-        printf("CRC16: 0x%04X\n", meta_crc);
-        obj_meta->meta_crc = meta_crc;
-        elf_flagdata(meta_data, ELF_C_SET, ELF_F_DIRTY);
-      }
-    }
-
-
-
-    elf_end(elf);
-//  } else {
-//    puts(elf_errmsg(-1));
-//  }
-
-  close(fd);
-
-#else
-  status = generate_crcs(elf_file, bin_file, verbose, /*apply_update*/!check_elf);
-#endif
-
-#if 0
-  int fd = open(elf_file, O_RDWR);
-  Elf *elf = elf_begin(fd, ELF_C_RDWR_MMAP, NULL);
-  elf_flagelf(elf, ELF_C_SET, ELF_F_LAYOUT);
-  Elf_Scn *meta_scn = get_named_section(elf, ".metadata");
-  Elf_Data *meta_data = elf_getdata(meta_scn, NULL);
-  ObjectMetadata *obj_meta = (ObjectMetadata *)meta_data->d_buf;
-
-
-  // Index program headers to map VMA to LMA
-  SegmentInfo *seg_index;
-  size_t prog_hdr_count = build_segment_index(elf, &seg_index);
-
-
-  puts("Regions:");
-  int region = 0;
-  for(int i = 0; i < OBJ_MAX_REGIONS; i++) {
-    if(obj_meta->mem_regions[i].end - obj_meta->mem_regions[i].start == 0)  // No more regions
-      break;
-
-    ObjMemRegion *mr = &obj_meta->mem_regions[i];
-    size_t offset;
-    const char *sname = find_lma_section(elf, mr->start, seg_index, prog_hdr_count, &offset);
-
-    printf("\ts: %08X,  e: %08X,  sz: %d,  %s\n", obj_meta->mem_regions[i].start,
-          obj_meta->mem_regions[i].end,
-          obj_meta->mem_regions[i].end - obj_meta->mem_regions[i].start,
-          sname);
-  }
-
-
-  puts("Sections:");
-  size_t string_table_ix;
-  elf_getshdrstrndx(elf, &string_table_ix);
-
-  // Read sections
-  Elf_Scn *scn = NULL;
-  GElf_Shdr shdr;
-  char *name;
-  while((scn = elf_nextscn(elf, scn)) != NULL) {
-    if(gelf_getshdr(scn, &shdr) == &shdr) {
-      if((name = elf_strptr(elf, string_table_ix, shdr.sh_name)) != NULL) {
-        Addr32 lma = get_lma(shdr.sh_addr, seg_index, prog_hdr_count);
-        size_t offset;
-        const char *sname = find_lma_section(elf, lma, seg_index, prog_hdr_count, &offset);
-        printf("%4jd  %16s  size= %6d %08X - %08X   %08X - %08X  %s\n" ,
-              (uintmax_t)elf_ndxscn(scn), name, shdr.sh_size,
-              shdr.sh_addr, shdr.sh_addr + shdr.sh_size, lma, lma + shdr.sh_size, sname);
-      }
-    }
-  }
-  
-  free(seg_index);
-#endif
-
-
-#if 0
-  for(size_t i = 0; i < prog_hdr_count; i++) {
-    GElf_Phdr phdr;
-    if(gelf_getphdr(elf, i, &phdr) == &phdr) {
-      printf("PHDR: %d %s\n", i, prog_hdr_type(phdr.p_type));
-#define PRINT_FIELD(field)  printf("\t%s: 0x%jX  (%d)\n", #field, phdr.field, phdr.field);
-      PRINT_FIELD(p_type);
-      PRINT_FIELD(p_vaddr);
-      PRINT_FIELD(p_paddr);
-      PRINT_FIELD(p_filesz);
-      PRINT_FIELD(p_memsz);
-      fputs("\tflags: ", stdout);
-      if(phdr.p_flags & PF_X) fputs("X", stdout);
-      if(phdr.p_flags & PF_R) fputs("R", stdout);
-      if(phdr.p_flags & PF_W) fputs("W", stdout);
-      puts("");
-    }
-  }
-
-  elf_end(elf);
-  close(fd);
-#endif
 
   if(bin_file_buf)
     free(bin_file_buf);
-
 
   return status;
 }
